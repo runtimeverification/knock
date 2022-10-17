83eeb176c99c3c533958ebedb1115007847e560e
hjort <8545447+hjorthjort@users.noreply.github.com>
2022-10-17 06:07:28 -0500

    clean up parser: curry takeList

--- parser.py
+++ parser.py
@@ -43,27 +43,29 @@ def takeInt(s):
     return (res, s)
 
 
-def takeList(s, mems_parser=None):
-    s = strip(s)
-    if len(s) > 0 and s[0] == '[':
-        res = []
-        s = s[1:]
-        while True:
-            (resTmp, s) = mems_parser(s)
-            if resTmp is None:
-                (lisRes, s) = takeList(s, mems_parser=mems_parser)
-                if lisRes is None:
-                    break
-                res.append(lisRes)
-            else:
-                res.append(resTmp)
-    else:
-        res = None
-    s = strip(s)
-    if len(s) > 0 and s[0] == ']':
-        s = strip(s[1:])
+def takeListOf(mems_parser):
+    def takeList(s):
+        s = strip(s)
+        if len(s) > 0 and s[0] == '[':
+            res = []
+            s = s[1:]
+            while True:
+                (resTmp, s) = mems_parser(s)
+                if resTmp is None:
+                    (lisRes, s) = takeList(s)
+                    if lisRes is None:
+                        break
+                    res.append(lisRes)
+                else:
+                    res.append(resTmp)
+        else:
+            res = None
+        s = strip(s)
+        if len(s) > 0 and s[0] == ']':
+            s = strip(s[1:])
 
-    return (res, s)
+        return (res, s)
+    return takeList
 
 
 def associate_right(l):
@@ -86,7 +88,7 @@ inp_file = sys.argv[1]
 with open(inp_file) as f:
     inp = f.read()
 leafParser = orParser(takeInt, takeSymb)
-listParser = lambda x: takeList(x, mems_parser=leafParser)
+listParser = takeListOf(leafParser)
 (res, s) = orParser(leafParser, listParser)(inp)
 if not s == '':
     print("Parse error:\nresult so far: %s\nremaining: %s" % (res, s))
