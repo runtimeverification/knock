a07276a003f7f53da1358a8d08c5fc411a8ce72d
hjort <8545447+hjorthjort@users.noreply.github.com>
2023-03-02 17:31:08 +0100

    script for prover

--- knock.sh
+++ knock.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 set -euxo pipefail
 
-kompile nock.k
 
-input="$1"
-shift
-python3 parser.py $input > $input.pre-parsed
-krun $input.pre-parsed "$@"
+function run {
+    kompile nock.k
+    input="$1"; shift
+    python3 parser.py $input > $input.pre-parsed
+    krun $input.pre-parsed "$@"
+}
+
+function prove {
+    kompile nock.k --backend haskell
+    input="$1"; shift
+
+    kprove $input "$@"
+}
+
+run_command="$1" ; shift
+
+case $run_command in
+    run    ) run     "$@" ;;
+    prove  ) prove   "$@" ;;
+    *      ) echo "Unknown command $run_command" ;;
+esac
+
+
+
 
