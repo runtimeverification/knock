bdebf3b76795e62c3cc55a3630f5eee2bf7317b2
Tamas Toth <tamas.toth@runtimeverification.com>
2023-03-08 13:58:16 +0100

    Fix formatting

--- src/knock/parser.py
+++ src/knock/parser.py
@@ -1,5 +1,4 @@
 import sys
-
 from typing import Any, Callable, List, Optional, Tuple, TypeVar, Union
 
 T = TypeVar('T')
@@ -18,18 +17,20 @@ def strip(s: str) -> str:
 
 def or_parser(p1: Parser[T1], p2: Parser[T2]) -> Parser[Union[T1, T2]]:
     """Create a parser that accepts one of two parsers"""
+
     def parse(s: str) -> Tuple[Union[None, T1, T2], str]:
         s = strip(s)
         (res_temp, s_temp) = p1(s)
         if res_temp is None:
             return p2(s)
         return (res_temp, s_temp)
+
     return parse
 
 
 def take_symb(s: str) -> ParseRes[str]:
     """Just a little hack for quick analysis: we allow the symbols "subject"
-    and "a", "b", "c", and "d". """
+    and "a", "b", "c", and "d"."""
     s = strip(s)
     if len(s) >= 7 and s[0:7] == 'subject':
         return ('subject', strip(s[7:]))
@@ -39,6 +40,8 @@ def take_symb(s: str) -> ParseRes[str]:
 
 
 int_syms = '0123456789.'
+
+
 def take_int(s: str) -> ParseRes[int]:
     s = strip(s)
     res_str = ''
@@ -74,6 +77,7 @@ def take_list_of(mems_parser: Parser) -> Parser[List]:
             s = strip(s[1:])
 
         return (res, s)
+
     return take_list
 
 
@@ -100,6 +104,6 @@ list_parser = take_list_of(leaf_parser)
 if not s == '':
     print('Parse error:\nresult so far: %s\nremaining: %s' % (res, s))
 res = associate_right(res)
-res = str(res).replace(',', '')  # we don't use commas to separate list items in nock.
-res = str(res).replace("'", '') # remove string quotes.
+res = str(res).replace(',', '')  # we don't use commas to separate list items in nock
+res = str(res).replace("'", '')  # remove string quotes
 print(res)
